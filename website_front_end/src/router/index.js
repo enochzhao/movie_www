021770e5889d91745882da8be0be9ab6ee7b2c4c
enochzhao <zypsalms@sina.com>
2019-09-12 21:20:45 +0800

    添加测试页面

--- website_front_end/src/router/index.js
+++ website_front_end/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import VueResource from 'vue-resource'
 import HelloWorld from '@/components/HelloWorld'
+import IndexPage from '@/pages/Index'
 
 Vue.use(Router)
 Vue.use(VueResource)
@@ -9,9 +10,17 @@ Vue.use(VueResource)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello_world/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'IndexPage',
+      component: resolve => require(['../pages/Index'], resolve),
+      meta: {
+        title: 'home'
+      }
     }
   ]
 })
