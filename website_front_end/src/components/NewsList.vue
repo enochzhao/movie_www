ab93e8f87f681a6f6cae2b9bb11510d1e509b911
enochzhao <zypsalms@sina.com>
2019-09-17 10:51:24 +0800

    Create NewsList.vue

---
+++ website_front_end/src/components/NewsList.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+    <li class="goods-list">
+      <div class="newsList">
+        <router-link :to="www.enochzhao.com" class="goods-list-link">假设为新闻-以诺君的方舟</router-link>
+      </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "NewsList"
+    }
+</script>
+
+<style lang="css" scoped>
+  .newsList{
+    padding: 5px;
+    font-size: 10px;
+    border-bottom: 1px dashed #ababab;
+  }
+</style>
