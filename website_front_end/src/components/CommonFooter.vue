85b5930f173058dc34063e252e50c54fc00f60a9
enochzhao <zypsalms@sina.com>
2019-09-17 09:49:28 +0800

    Create CommonFooter.vue

---
+++ website_front_end/src/components/CommonFooter.vue
@@ -0,0 +1,30 @@
+<templaten lang="html">
+  <div class="footer">
+    <div class="footer">
+      <p>vue.js实例</p>
+    </div>
+  </div>
+</templaten>
+
+<script>
+    export default {
+        name: "CommonFooter"
+    }
+</script>
+
+<style lang="css" scoped>
+  .footer{
+    height: 60px;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    border-top: 2px solid #ccc;
+    background-color: #000;
+  }
+  .footer p{
+    font-size: 10px;
+    color: #fff;
+    text-align: center;
+  }
+</style>
