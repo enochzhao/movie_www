0aa73ae6fd54e0740915e73e2f2f68077956e6a1
enochzhao <zypsalms@sina.com>
2019-09-17 10:23:04 +0800

    Create IndexHeaderPic.vue

---
+++ website_front_end/src/components/IndexHeaderPic.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+    <div class="headerPic">
+      <div>
+        <p class="imgTitle">{{recommendTitle}}</p>
+        <a href="baidu.com">
+          <img src="../assets/pic2.jpg" alt="-*^*-!" class="headerImg">
+        </a>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "IndexHeaderPic"
+    }
+</script>
+
+<style lang="css" scoped>
+  .headerImg{
+    height: 300px;
+    width: 100%;
+  }
+  .imgTitle{
+    z-index: 2;
+    padding-left: 45%;
+    padding-top: 230px;
+    position: absolute;
+    color: #fff;
+    font-size: 20px;
+  }
+</style>
