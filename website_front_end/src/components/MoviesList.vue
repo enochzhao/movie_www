69a63d26194d2097669925467176d6671d04b03f
enochzhao <zypsalms@sina.com>
2019-09-17 10:37:11 +0800

    Create MoviesList.vue

---
+++ website_front_end/src/components/MoviesList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+    <div class="movieList">
+      <div>
+        <router-link :to="www.enochzhao.com" class="goods-list-link">假设为电影-以诺君的方舟</router-link>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MoviesList"
+    }
+</script>
+
+<style lang="css" scoped>
+  .movieList{
+    padding: 5px;
+    border-bottom: 1px dashed #ababab;
+  }
+</style>
