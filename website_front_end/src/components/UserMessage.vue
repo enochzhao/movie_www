3f1e19c4a9f73fb666ec8d5fb7081970b88ecbf9
enochzhao <zypsalms@sina.com>
2019-09-17 09:58:40 +0800

    Create UserMessage.vue

---
+++ website_front_end/src/components/UserMessage.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+    <div class="header">
+      <router-link to="/loginPage">
+        <div class="header_menu">登录</div>
+      </router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserMessage"
+    }
+</script>
+
+<style lang="css" scoped>
+  .header{
+    width: 103%;
+    height: 30px;
+    left: 0;
+    top: 0;
+    color: #000;
+    background-color: #C3BD5C;
+  }
+    .header_menu{
+      padding-right: 60px;
+      padding-top: 10px;
+      float: right;
+      color: #fff;
+      font-size: 8px;
+    }
+</style>
